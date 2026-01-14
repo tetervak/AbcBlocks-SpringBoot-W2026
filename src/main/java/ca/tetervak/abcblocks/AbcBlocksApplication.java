1a624b2a3a8de5739edf458c13ee3bb164e90402
Alex Tetervak <tetervak@sheridancollege.ca>
2026-01-14 17:25:38 -0500

    initial commit

---
+++ src/main/java/ca/tetervak/abcblocks/AbcBlocksApplication.java
@@ -0,0 +1,13 @@
+package ca.tetervak.abcblocks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbcBlocksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AbcBlocksApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ca/tetervak/abcblocks/AbcBlocksApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.tetervak.abcblocks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbcBlocksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
